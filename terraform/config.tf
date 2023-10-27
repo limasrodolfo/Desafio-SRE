3f4a15a311efd9decb561ca40285b788b4925769
limasrodolfo <lima.s.rodolfo@gmail.com>
2023-10-27 18:17:56 -0300

    terraform

--- terraform/config.tf
+++ terraform/config.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = ">=1.1.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.22.0"
     }
   }
@@ -12,12 +12,12 @@ terraform {
 // Declaração do provider AWS
 provider "aws" {
   profile = "desafio"   # Define o perfil que contém usuário e senha de acesso na aws
-  region = "us-east-1"  # Define a Região
-  
+  region  = "us-east-1" # Define a Região
+
   default_tags {
     tags = {
       managed-by = "terraform" # Tag default para todos os serviços terraform
     }
-    
+
   }
 }
